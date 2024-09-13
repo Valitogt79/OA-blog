a1d3cf21e51a128237ae1c9a3987d4da85c47257
Oswaldo Alonzo <oalonzo@gmail.com>
2024-09-13 17:02:19 -0600

    Utilizando Astro.glob y sus props

---
+++ src/utils/formatter.ts
@@ -0,0 +1,13 @@
+export class Formatter {
+
+  static formatDate(value: Date): string {
+
+    const date = new Date(value);
+
+    return Intl.DateTimeFormat('es-ES', {
+      year: 'numeric',
+      month: 'long',
+      day: '2-digit'
+    }).format(date)
+  }
+}
